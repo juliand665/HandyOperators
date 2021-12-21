23c1c9f78fbd4f1f1a3b9f88c6fddb199c8726b2
Julian Dunskus <julian.dunskus@gmail.com>
2021-12-21 20:23:58 +0100

    make inlinable
    can't believe i didn't know about this

--- Sources/HandyOperators/HandyOperators.swift
+++ Sources/HandyOperators/HandyOperators.swift
@@ -10,12 +10,14 @@ precedencegroup WithPrecedence {
 /**
 Often called `with`, this function allows you to apply extra transformations to something without creating a `var` to mutate.
 
-Especially useful when configuring constants, e.g. ```
+Especially useful when configuring constants, e.g.:
+ ```swift
 let dateFormatter = DateFormatter() <- {
 	$0.timeStyle = .long
 }
 ```
 */
+@inlinable
 @discardableResult
 public func <- <T>(
 	object: T,
@@ -35,6 +37,7 @@ Takes an optional, returning its wrapped value if present and throwing the given
 
 The error is wrapped in an `@autoclosure`, so it's not evaluated unless it's actually thrown.
 */
+@inlinable
 public func ??? <Wrapped>(
 	optional: Wrapped?,
 	error: @autoclosure () throws -> Error
